d5912f8a9bab544fdb03eb319360312c1efe5126
VishuPathania <vishalpathania0003@gmail.com>
2023-08-27 22:02:02 +0530

    Addressing security and logout issues using Local Storage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,15 +9,18 @@ const AuthContext =React.createContext( {
 });
 
 export const AuthContextProvider= (props) => {
-      const [token, setToken] =useState(null);
+      const initialToken = localStorage.getItem('token'); // to change and use prior token its either UNDEFIEND or token
+      const [token, setToken] =useState(initialToken);
       const userIsLoggedIn = !!token; //if token return non emptystring so TRUE
 
-      const loginHandler = () => {
+      const loginHandler = (token) => {    
         setToken(token);
-      }
+        localStorage.setItem('token' ,token);
+      };
 
       const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
       };
 
       const contextValue  = {
